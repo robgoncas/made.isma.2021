f87b51ebccc4793d246f3606fdc03369a184df81
robgoncas <rob.gonzalez.castro@gmail.com>
2021-10-13 14:12:46 -0300

    ultima actualizacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -423,10 +423,10 @@
         = COUNTDOWN CLOCK
     -------------------------------------------*/
     if ($("#clock").length) {
-        $('#clock').countdown('2020/12/31', function(event) {
+        $('#clock').countdown('2021/10/30', function(event) {
             var $this = $(this).html(event.strftime(''
-            + '<div class="box"><div>%D</div> <span>Days</span> </div>'
-            + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
+            + '<div class="box"><div>%D</div> <span>Días</span> </div>'
+            + '<div class="box"><div>%H</div> <span>Horas</span> </div>'
             + '<div class="box"><div>%M</div> <span>Mins</span> </div>'
             + '<div class="box"><div>%S</div> <span>Secs</span> </div>'));
         });
